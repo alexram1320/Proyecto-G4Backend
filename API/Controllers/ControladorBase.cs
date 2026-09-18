cf2131beb8c975eb8190e5195658f59898725fe1
AldairOrellana <64727634+AldariOrellana@users.noreply.github.com>
2026-09-18 02:51:35 -0600

    swagger recibe header y y controlador base body

--- API/Controllers/ControladorBase.cs
+++ API/Controllers/ControladorBase.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace API.Controllers
 {
+    [ApiController]
     public abstract class ControladorBase : ControllerBase
     {
         protected IActionResult Responder<T>(Respuesta<T> respuesta) => StatusCode(respuesta.CodigoEstado, respuesta);
